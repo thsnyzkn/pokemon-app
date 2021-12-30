eaf243bf8d9e2fc0f5ef4fdcb8cce6d94bff28e9
thsnyzkn <tahsinyazkan@gmail.com>
2021-12-30 12:12:57 +0300

    fetch first generation of Pokemons data and render its list.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=151"
+        );
+        const data = await response.json();
+
+        setPokemons(data?.results);
+        console.log(data?.results);
+      } catch (error) {
+        setError(true);
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
+  if (loading) {
+    <div>Loading...</div>;
+  }
+  if (error) {
+    <div>Error occurred..</div>;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <ul>
+          {pokemons.map(({ name, image }, index) => (
+            <li key={index}>
+              <span>{name}</span>
+              <div>
+                <img
+                  src={`https://img.pokemondb.net/sprites/black-white/anim/normal/${name}.gif`}
+                  alt={`${name}'s visual depiction`}
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
